b14774a822db56a8bc0a7fb8afcd2fe7888fc654
msivanesan <msivanesan2003@gmail.com>
2024-02-03 22:05:26 +0530

    department ,principal panal completed

--- councling/myapp/extendedviews.py
+++ councling/myapp/extendedviews.py
@@ -27,4 +27,53 @@ def constudent(request,department, userid):
         # Log the exception for debugging
         # Return a generic error message or redirect to an error page
         return HttpResponse(e)
+    
+def department(request,department):
+    try:
+        dpt=models.Department.objects.get(name=department)
+        data=models.PgStudentDetails.objects.order_by('-percentageoptained')
+        context={
+            'data':data,
+            'department':dpt
+        }
+        return render(request,'department/listpage.html',context)
+    except Exception as e:
+        return HttpResponse(e)
+    
+def depstudent(request,department, userid):
+    try:
+        data = models.PgStudentDetails.objects.filter(student__username=userid).filter(details_submited=True)
+        context = {'data': data}
+        return render(request, 'department/listpage.html', context)
+    except models.PgStudentDetails.DoesNotExist:
+        raise Http404("Student details not found.")
+    except Exception as e:
+        # Log the exception for debugging
+        # Return a generic error message or redirect to an error page
+        return HttpResponse(e)
+    
+
+def principal(request):
+    try:
+        dpt=models.Department.objects.all()
+        data=models.PgStudentDetails.objects.order_by('-percentageoptained')
+        context={
+            'data':data,
+            'department':dpt
+        }
+        return render(request,'principal/listpage.html',context)
+    except Exception as e:
+        return HttpResponse(e)
+    
+def pplstudent(request, userid):
+    try:
+        data = models.PgStudentDetails.objects.filter(student__username=userid).filter(details_submited=True)
+        context = {'data': data}
+        return render(request, 'department/listpage.html', context)
+    except models.PgStudentDetails.DoesNotExist:
+        raise Http404("Student details not found.")
+    except Exception as e:
+        # Return a generic error message or redirect to an error page
+        return HttpResponse(e)
+
 
--- councling/myapp/urls.py
+++ councling/myapp/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('opt-verfiy',views.otp_auth,name='otp_auth'),
     path('getpgdata',views.pgregister,name='pgregister'),
     path('controler/<str:department>/',view.controller,name='controler'),
-    path('controler/<str:department>/<str:userid>',view.constudent,name="constudent")
+    path('controler/<str:department>/<str:userid>',view.constudent,name="constudent"),
+    path('department/<str:department>/',view.department,name='department'),
+    path('department/<str:department>/<str:userid>',view.depstudent,name="depstudent"),
+    path('principal/',view.principal,name='principal'),
+    path('principal/<str:userid>',view.pplstudent,name="pplstudent"),
+
 ]
